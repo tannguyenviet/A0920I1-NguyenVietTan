3d8c8f827f2208149c54683b074241cfc4957211
KTan <nguyenviettantan@gmail.com>
2020-10-03 19:52:53 +0700

    ad ex

---
+++ 17. Lập trình hướng đối tượng/baitap/tivi/index.js
@@ -0,0 +1,16 @@
+function Remote(){
+    this.buttonChanel =0;
+    this.buttonVolume =69;
+    this.connect=function(tivi){
+        
+    }
+    this.switchChanel = function(){
+
+    }
+}
+function Tivi(){
+    this.status = false
+    this.chanel=0;
+    this.volume =69;
+    this.
+}
